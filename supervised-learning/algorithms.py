53452edf38d4f8dc5814b84a5c581bf58c81505c
Tyler Rarick <tyler.rarick@gmail.com>
2020-09-20 11:47:57 -0500

    Added more algs and experiments and updated gitignore

--- supervised-learning/algorithms.py
+++ supervised-learning/algorithms.py
@@ -1,12 +1,21 @@
+import numpy as np
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import log_loss
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 
 
 # Normalize inputs
+#
+# Assumes final column is the only output
 def normalize_dataset_inputs(dataset):
     new_dataset = dataset.copy()
 
     inputs = new_dataset.iloc[:,:-1]
-    new_dataset.iloc[:,:-1] = (inputs - inputs.mean()) / inputs.std()
+    new_dataset.iloc[:,:-1] = (inputs - inputs.mean()) / (2*inputs.std())
 
     return new_dataset
 
@@ -22,3 +31,109 @@ def split_data(dataset, validation_size=0.2, test_size=0.1):
     training, validation = train_test_split(training, test_size=validation_size)
 
     return training, validation, test
+
+
+class DecisionTree(object):
+
+    def __init__(self, alpha):
+        self.dt = DecisionTreeClassifier(criterion='entropy', ccp_alpha=alpha)
+
+
+    def train(self, dataset):
+        self.dt = self.dt.fit(dataset.iloc[:, :-1], dataset.iloc[:, -1])
+        return self
+
+
+    def predict(self, inputs):
+        return self.dt.predict(inputs)
+
+
+    def score(self, dataset):
+        outputs = self.predict(dataset.iloc[:, :-1])
+        return (dataset.iloc[:, -1] == outputs).mean()
+
+
+class NeuralNetwork(object):
+
+    def __init__(self, hidden_size, hidden_depth):
+        self.nn = MLPClassifier(hidden_layer_sizes=[hidden_size]*hidden_depth,
+                                learning_rate_init=1e-2)
+
+
+    def train(self, dataset):
+        self.nn = self.nn.fit(dataset.iloc[:, :-1], dataset.iloc[:, -1])
+        return self
+
+
+    def predict(self, inputs):
+        return self.nn.predict(inputs)
+
+
+    def score(self, dataset):
+        outputs = self.predict(dataset.iloc[:, :-1])
+        return (dataset.iloc[:, -1] == outputs).mean()
+
+
+class Boosting(object):
+
+    def __init__(self, alpha):
+        self.rf = RandomForestClassifier(ccp_alpha=alpha)
+
+
+    def train(self, dataset):
+        self.rf = self.rf.fit(dataset.iloc[:, :-1], dataset.iloc[:, -1])
+        return self
+
+
+    def predict(self, inputs):
+        return self.rf.predict(inputs)
+
+
+    def score(self, dataset):
+        outputs = self.predict(dataset.iloc[:, :-1])
+        return (dataset.iloc[:, -1] == outputs).mean()
+
+
+class SupportVectorMachine(object):
+
+    def __init__(self, kernel):
+        self.svm = SVC(kernel=kernel)
+
+
+    def train(self, dataset):
+        self.svm = self.svm.fit(dataset.iloc[:, :-1], dataset.iloc[:, -1])
+        return self
+
+
+    def predict(self, inputs):
+        return self.svm.predict(inputs)
+
+
+    def score(self, dataset):
+        outputs = self.predict(dataset.iloc[:, :-1])
+        return (dataset.iloc[:, -1] == outputs).mean()
+
+
+class NearestNeighbors(object):
+
+    def __init__(self, k):
+        self.knn = NearestNeighbors(n_neighbors=k)
+
+
+    def train(self, dataset):
+        self.knn = self.knn.fit(dataset.iloc[:, :-1], dataset.iloc[:, -1])
+        return self
+
+
+    def classify_query(results):
+        return np.bincount(results).argmax()
+
+
+    def predict(self, inputs):
+        results = self.knn.kneighbors(inputs)
+        return [classify_query(x) for x in results]
+
+
+    def score(self, dataset):
+        outputs = self.predict(dataset.iloc[:, :-1])
+        return (dataset.iloc[:, -1] == outputs).mean()
--- supervised-learning/main.py
+++ supervised-learning/main.py
@@ -1,11 +1,143 @@
+#!/usr/bin/python3
+
+import algorithms as alg
 import pandas as pd
 import numpy as np
-import algorithms as alg
+import itertools
 
+from matplotlib import pyplot as plt
 from pathlib import Path
+from tqdm import tqdm
 
 
+np.random.seed(seed=1337)
 datasets_path = Path('../datasets')
+
+
+def ex_1(wine_train, wine_val, heart_train, heart_val):
+    wine_scores = []
+    heart_scores = []
+    for alpha in np.linspace(0, 0.05, 20):
+        heart_dt = alg.DecisionTree(alpha)
+        heart_dt.train(heart_train)
+        accuracy = heart_dt.score(heart_val)
+        heart_scores.append({'alpha': alpha,
+                             'accuracy': accuracy})
+
+        wine_dt = alg.DecisionTree(alpha)
+        wine_dt.train(wine_train)
+        accuracy = wine_dt.score(wine_val)
+        wine_scores.append({'alpha': alpha,
+                            'accuracy': accuracy})
+
+    heart_results = pd.DataFrame.from_records(heart_scores)
+    wine_results = pd.DataFrame.from_records(wine_scores)
+
+    heart_results.plot.scatter(x='alpha', y='accuracy')
+    plt.ylim(bottom=0)
+    plt.grid()
+    plt.savefig('./ex1_heart.png')
+    wine_results.plot.scatter(x='alpha', y='accuracy')
+    plt.ylim(bottom=0)
+    plt.grid()
+    plt.savefig('./ex1_wine.png')
+
+
+def ex_2(wine_train, wine_val, heart_train, heart_val):
+    wine_scores = []
+    heart_scores = []
+    combinations = itertools.product([2, 3, 4], [128, 256, 512])
+    for hidden_layers, hidden_size in tqdm(combinations, total=9):
+        heart_nn = alg.NeuralNetwork(hidden_size, hidden_layers)
+        heart_nn.train(heart_train)
+        accuracy = heart_nn.score(heart_val)
+        heart_scores.append({'hidden_layers': hidden_layers,
+                             'hidden_size': hidden_size,
+                             'accuracy': accuracy})
+
+        wine_nn = alg.NeuralNetwork(hidden_size, hidden_layers)
+        wine_nn.train(wine_train)
+        accuracy = wine_nn.score(wine_val)
+        wine_scores.append({'hidden_layers': hidden_layers,
+                            'hidden_size': hidden_size,
+                            'accuracy': accuracy})
+
+    heart_results = pd.DataFrame.from_records(heart_scores)
+    wine_results = pd.DataFrame.from_records(wine_scores)
+
+    def nn_plot(df):
+        colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+        fig, ax = plt.subplots()
+        for key, grp in df.groupby(['hidden_layers']):
+            ax = grp.plot.scatter(ax=ax,
+                                  x='hidden_size',
+                                  y='accuracy',
+                                  label=f'{key} hidden layers',
+                                  color=colors.pop(0))
+        plt.show()
+
+    nn_plot(heart_results)
+    plt.ylim(bottom=0)
+    plt.grid()
+    plt.savefig('./ex2_heart.png')
+    nn_plot(wine_results)
+    plt.ylim(bottom=0)
+    plt.grid()
+    plt.savefig('./ex2_wine.png')
+
+
+def ex_3(wine_train, wine_val, heart_train, heart_val):
+    wine_scores = []
+    heart_scores = []
+    for alpha in np.linspace(0, 0.05, 20):
+        heart_dt = alg.Boosting(alpha)
+        heart_dt.train(heart_train)
+        accuracy = heart_dt.score(heart_val)
+        heart_scores.append({'alpha': alpha,
+                             'accuracy': accuracy})
+
+        wine_dt = alg.DecisionTree(alpha)
+        wine_dt.train(wine_train)
+        accuracy = wine_dt.score(wine_val)
+        wine_scores.append({'alpha': alpha,
+                            'accuracy': accuracy})
+
+    heart_results = pd.DataFrame.from_records(heart_scores)
+    wine_results = pd.DataFrame.from_records(wine_scores)
+
+    heart_results.plot.scatter(x='alpha', y='accuracy')
+    plt.ylim(bottom=0)
+    plt.grid()
+    plt.savefig('./ex3_heart.png')
+    wine_results.plot.scatter(x='alpha', y='accuracy')
+    plt.ylim(bottom=0)
+    plt.grid()
+    plt.savefig('./ex3_wine.png')
+
+
+def ex_4(wine_train, wine_val, heart_train, heart_val):
+    wine_scores = []
+    heart_scores = []
+    for kernel in tqdm(['linear', 'poly', 'rbf']):
+        heart_svm = alg.SupportVectorMachine(kernel)
+        heart_svm.train(heart_train)
+        accuracy = heart_svm.score(heart_val)
+        heart_scores.append({'kernel': kernel,
+                             'accuracy': accuracy})
+
+        wine_svm = alg.SupportVectorMachine(kernel)
+        wine_svm.train(wine_train)
+        accuracy = wine_svm.score(wine_val)
+        wine_scores.append({'kernel': kernel,
+                             'accuracy': accuracy})
+
+    heart_results = pd.DataFrame.from_records(heart_scores)
+    wine_results = pd.DataFrame.from_records(wine_scores)
+
+    heart_results.plot.bar(x='kernel', y='accuracy')
+    plt.savefig('./ex4_heart.png')
+    wine_results.plot.bar(x='kernel', y='accuracy')
+    plt.savefig('./ex4_wine.png')
 
 
 def main():
@@ -18,9 +150,11 @@ def main():
     wine_train, wine_val, wine_test = alg.split_data(wine_data)
     heart_train, heart_val, heart_test = alg.split_data(heart_data)
 
-    print(wine_train.shape)
-    print(wine_val.shape)
-    print(wine_test.shape)
+    # ex_1(wine_train, wine_val, heart_train, heart_val)
+    # ex_2(wine_train, wine_val, heart_train, heart_val)
+    # ex_3(wine_train, wine_val, heart_train, heart_val)
+    # ex_4(wine_train, wine_val, heart_train, heart_val)
+    ex_5(wine_train, wine_val, heart_train, heart_val)
 
 
 if __name__ == '__main__':
