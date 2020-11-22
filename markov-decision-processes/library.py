7ad3592b873803a4d90342aa9dc07ddae1d77d98
Tyler Rarick <tyler.rarick@gmail.com>
2020-11-22 12:03:58 -0600

    Commiting before trying discrete car

--- markov-decision-processes/library.py
+++ markov-decision-processes/library.py
@@ -7,19 +7,33 @@ from timeit import default_timer as timer
 from tqdm import tqdm
 
 
+# num_positions=128
+# num_velocities=128
+# num_actions=8
+
+num_positions=63
+num_velocities=63
+num_actions=8
+
+
 def run():
     P, R = sample_mountain_car()
 
     vi = value_iteration(P, R)
     print(vi.run_stats[-1])
+    get_performance(vi.policy)
+
     pi = policy_iteration(P, R)
     print(pi.run_stats[-1])
+    get_performance(pi.policy)
+
     ql = q_learning(P, R)
     print(ql.run_stats[-1])
+    get_performance(ql.policy)
 
 
-def sample_mountain_car(num_positions=127, num_velocities=63, num_actions=15):
-# def sample_mountain_car(num_positions=127, num_velocities=31, num_actions=7):
+# def sample_mountain_car(num_positions=127, num_velocities=63, num_actions=15):
+def sample_mountain_car():
     num_states = num_positions*num_velocities
 
     env = gym.make('MountainCarContinuous-v0')
@@ -77,10 +91,6 @@ def sample_mountain_car(num_positions=127, num_velocities=63, num_actions=15):
         P[a, s, sp] += 1
         R[s, a] = r
 
-    # Should not be necessary, all S,A pairs were tried
-    # empty = P.sum(axis=2) == 0
-    # P[empty, 0] = 1
-
     assert (P.sum(axis=2) == 1).all()
 
     return P, R
@@ -111,9 +121,51 @@ def policy_iteration(P, R):
 def q_learning(P, R):
     print('Q Learning')
     start = timer()
-    ql = mdptoolbox.mdp.QLearning(P, R, 0.9, alpha_decay=0.999, alpha_min=0.5, epsilon_min=0.2, epsilon_decay=0.999)
+    ql = mdptoolbox.mdp.QLearning(P, R, 0.9, alpha_decay=0.999, alpha_min=0.1, epsilon_min=0.2, epsilon_decay=0.999)
     ql.setVerbose()
     ql.run()
     end = timer()
     print(end - start)
     return ql
+
+
+def get_performance(policy, num_trials=10):
+    env = gym.make('MountainCarContinuous-v0')
+    env.reset()
+
+    num_states = num_positions*num_velocities
+    assert len(policy) == num_states
+
+    action_space = env.action_space
+    observation_space = env.observation_space
+
+    position_min, velocity_min = observation_space.low
+    position_max, velocity_max = observation_space.high
+
+    action_min, action_max = (action_space.low[0], action_space.high[0])
+
+    positions = np.linspace(position_min, position_max, num_positions)
+    velocities = np.linspace(velocity_min, velocity_max, num_velocities)
+    action_space = np.linspace(action_min, action_max, num_actions)
+
+    trial_data = []
+    for k in range(num_trials):
+        env = gym.make('MountainCarContinuous-v0')
+        state = env.reset()
+        total_reward = 0
+        while True:
+            position_index = np.digitize(state[0], positions, right=True)
+            velocity_index = np.digitize(state[1], velocities, right=True)
+            state_index = num_velocities*position_index + velocity_index
+
+            action_index = policy[state_index]
+            action = action_space[action_index]
+            print(f'{(state, action)}')
+            state, reward, done, _ = env.step([action])
+            total_reward += reward
+            if done:
+                break
+
+        trial_data.append(total_reward)
+
+    print(trial_data)
